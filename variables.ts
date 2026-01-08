6c9927741667cd14745b412ff81499e337989ae9
Roberto Pinho <roberto.pinho@protonmail.com>
2026-01-08 18:40:43 -0300

    Add basic TypeScript variable declarations

---
+++ variables.ts
@@ -0,0 +1,13 @@
+export {}; 
+const nome: string = 'Roberto';
+let age: number = 40;
+const profession: string = 'Software engineer';
+const use_typescript: boolean = true;
+let rating: number  = 10;
+
+
+console.log(nome)
+console.log(age)
+console.log(profession)
+console.log(use_typescript)
+console.log(rating)
